0c15473590db3e06d9b661034a439495a2b931a7
JerryMissTom <582486638@qq.com>
2017-12-01 10:11:21 +0800

    添加url管理和util服务

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HttpService } from './http.service';
 import { ErrorService } from './error.service';
+import { BroadcastService } from './broadcast.service';
+import { UtilsService } from './utils.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [],
-  providers: [HttpService, ErrorService]
+  providers: [HttpService, ErrorService, BroadcastService, UtilsService]
 })
 export class CoreModule { }
