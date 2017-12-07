a512efb3a6f67e917d7896ad461679b70ac9ab17
JerryMissTom <582486638@qq.com>
2017-12-07 16:15:19 +0800

    更新了主界面

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'hw-admin',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  loginOut() {
+    this.router.navigate(['login'], { replaceUrl: true }); // 点击浏览器的后退按钮，无法返回至登录页
   }
 
 }
